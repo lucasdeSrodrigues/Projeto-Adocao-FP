15dcd2182a6562e45aa632d21ce10a1e9fa78a3b
Rafael <rafaelgurgel1708@gmail.com>
2026-06-05 12:32:58 -0300

    implemente

--- topico3.py
+++ topico3.py
@@ -7,17 +7,17 @@ def calcular_dias_restantes(data_prevista):
     return diferenca.days
 
 def mostrar_contagem_regressiva(animal, cuidados):
-    print(f'\nPróximas tarefas para {animal}:')
+    print(f'\n   Próximas tarefas para {animal}:')
     encontrou = False
     for cuidado in cuidados:
         if cuidado['animal'].lower() == animal.lower():
             encontrou = True
             dias_restantes = calcular_dias_restantes(cuidado['data'])
             if dias_restantes > 0:
-                print(f"- {cuidado['tipo']} em {cuidado['data']} (Faltam {dias_restantes} dias)")
+                print(f"   - {cuidado['tipo']} em {cuidado['data']} (Faltam {dias_restantes} dias)\n")
             if dias_restantes < 0:
-                print(f" - {cuidado['tipo']} está atrasado! Por favor, realize o mais rápido possível.")
+                print(f"   - {cuidado['tipo']} está atrasado! Por favor, realize o mais rápido possível.\n")
             elif dias_restantes == 0:
-                print(f" - {cuidado['tipo']} está previsto para hoje!")
+                print(f"   - {cuidado['tipo']} está previsto para hoje!\n")
     if not encontrou:
-        print ('Nenhuma tarefa encontrada para este animal.')
+        print ('Nenhuma tarefa encontrada para este animal.\n')
